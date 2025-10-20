a25997ec028064ed059cbed911b3c1f429b21e59
Thomas Pal <thomas.pal@tuhh.de>
2025-10-20 21:30:45 +0200

    Move cascade neumorphistic shadows into separate Modifier extensions

--- lib/src/commonMain/kotlin/dev/tjpal/foundation/themes/cascade/CardConfiguration.kt
+++ lib/src/commonMain/kotlin/dev/tjpal/foundation/themes/cascade/CardConfiguration.kt
@@ -1,31 +1,15 @@
 package dev.tjpal.foundation.themes.cascade
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.tjpal.foundation.themes.tokens.CardBasicConfiguration
 import dev.tjpal.foundation.themes.tokens.CardHeaderConfiguration
 import dev.tjpal.foundation.themes.tokens.CardTokens
 
-private val CardCornerRadius = 6f.dp
-
 fun createCardConfiguration(): CardTokens {
     return CardTokens(
         basicConfiguration = CardBasicConfiguration(
-            modifier = Modifier.
-            shapeShadow(
-                highlightShadowColor = HighlightShadowColor,
-                castShadowColor = CastShadowColor,
-                blurRadius = OutsetShadowBlurRadius,
-                offsetY = OutsetShadowOffsetX,
-                offsetX = OutsetShadowOffsetY,
-                shape = RoundedCornerShape(CardCornerRadius)
-            ).
-            background(
-                BackgroundColor,
-                RoundedCornerShape(CardCornerRadius)
-            ),
+            modifier = Modifier.defaultCascadeShapeShadow().defaultCascadeBackground(),
             fundamentalPadding = 8.dp,
             cardTileSize = (64 + 8).dp,
         ),
