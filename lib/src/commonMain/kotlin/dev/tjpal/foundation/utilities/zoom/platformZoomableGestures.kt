2c2aa233514ebd694d8a0a5216a8db5cde01dfef
Thomas Pal <thomas.pal@tuhh.de>
2025-10-23 19:01:02 +0200

    Move zoomableGestures to commonMain
    Platform code will remain independent for now. Also, it's not easy to export
    it to the published library.

--- lib/src/commonMain/kotlin/dev/tjpal/foundation/utilities/zoom/platformZoomableGestures.kt
+++ lib/src/commonMain/kotlin/dev/tjpal/foundation/utilities/zoom/platformZoomableGestures.kt
@@ -1,14 +1,33 @@
 package dev.tjpal.foundation.utilities.zoom
 
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.ui.Modifier
-
-// Platform specific implementations exist in their respective source sets
-expect fun Modifier.platformZoomableGestures(
-    state: ZoomableState,
-    config: GestureConfig = GestureConfig()
-): Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.pointerInput
 
 fun Modifier.zoomableGestures(
     state: ZoomableState,
     config: GestureConfig = GestureConfig()
-): Modifier = platformZoomableGestures(state, config)
+): Modifier = this.pointerInput(state, config) {
+    awaitPointerEventScope {
+        while (true) {
+            val event = awaitPointerEvent()
+
+            if (event.type == PointerEventType.Scroll) {
+                val scrollDelta = event.changes.firstOrNull()?.scrollDelta ?: Offset.Zero
+
+                if (config.wheelZooms) {
+                    val deltaScale = 1f + (-scrollDelta.y) * config.wheelZoomFactor
+                    state.onTransformDelta(deltaScale, Offset.Zero, null)
+                }
+
+                event.changes.forEach { it.consume() }
+            }
+        }
+    }
+}.pointerInput(state, config) {
+    detectTransformGestures { centroid, pan, zoom, _ ->
+        state.onTransformDelta(zoom, Offset(pan.x, pan.y), centroid)
+    }
+}
