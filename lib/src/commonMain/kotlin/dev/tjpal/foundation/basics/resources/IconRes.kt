919b3aa81d4d81cf666286d65e03719061642f56
tjpal <50217098+tjpal@users.noreply.github.com>
2025-11-09 11:56:33 +0100

    Add icon res composable to render icons in IconButtons

---
+++ lib/src/commonMain/kotlin/dev/tjpal/foundation/basics/resources/IconRes.kt
@@ -0,0 +1,17 @@
+package dev.tjpal.foundation.basics.resources
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+
+
+@Composable
+fun IconRes(resource: DrawableResource, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(resource),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
