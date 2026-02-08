5b70691fd6454315bfaccd872af3765bcfd843bb
tjpal <50217098+tjpal@users.noreply.github.com>
2026-02-08 08:59:59 +0100

    Text API exposes parameters to handle text overflow

--- lib/src/commonMain/kotlin/dev/tjpal/composition/foundation/basics/text/Text.kt
+++ lib/src/commonMain/kotlin/dev/tjpal/composition/foundation/basics/text/Text.kt
@@ -3,6 +3,8 @@ package dev.tjpal.composition.foundation.basics.text
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.style.TextOverflow
 import dev.tjpal.composition.foundation.themes.tokens.TextType
 import dev.tjpal.composition.foundation.themes.tokens.Theme
 import dev.tjpal.composition.foundation.themes.tokens.Typography
@@ -11,20 +13,40 @@ import dev.tjpal.composition.foundation.themes.tokens.Typography
 fun Text(
     text: String,
     type: TextType = TextType.DEFAULT,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    maxLines: Int = Int.MAX_VALUE,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    onTextLayout: (TextLayoutResult) -> Unit = {}
 ) {
-    Text(text, Theme.current.typography.getTypography(type), modifier)
+    Text(
+        text = text,
+        typography = Theme.current.typography.getTypography(type),
+        modifier = modifier,
+        maxLines = maxLines,
+        overflow = overflow,
+        softWrap = softWrap,
+        onTextLayout = onTextLayout
+    )
 }
 
 @Composable
 fun Text(
     text: String,
     typography: Typography,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    maxLines: Int = Int.MAX_VALUE,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    onTextLayout: (TextLayoutResult) -> Unit = {}
 ) {
     BasicText(
         text = text,
         style = typography.style,
-        modifier = modifier
+        modifier = modifier,
+        maxLines = maxLines,
+        overflow = overflow,
+        softWrap = softWrap,
+        onTextLayout = onTextLayout
     )
 }
